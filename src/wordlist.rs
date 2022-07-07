0b8ba268dfaf037521c444ddd38a71b4ab843731
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-07-07 17:50:45 -0400

    Wordlist as a Trait
    Add Wordlist trait and one implementor (BorrowedWordList)

--- src/wordlist.rs
+++ src/wordlist.rs
@@ -1,120 +1,83 @@
-//! The `Wordlist` struct and associated utilities
-
-use std::{io::{self, BufRead}, fs, path};
-
-
-/// an enum to allow either a Vec<String> or Vec<&str>
-enum WordlistStorage<'a> 
-{
-    Owned(Vec<String>),
-    Borrowed(Vec<&'a str>)
-}
+//! The `Wordlist` Trait and some implementations
 
 /// A list of words
-pub struct Wordlist<'a> {
-    word_set: WordlistStorage<'a>
+/// 
+/// A `Wordlist` is a list of words (each word being a `&str`).
+pub trait Wordlist<'a>
+{
+    /// The type of iterator that the `iter` method returns
+    /// 
+    /// Must be an [Iterator] yielding `&str`
+    type IterType: Iterator<Item = &'a str>;
+
+    /// Returns an an iterator that returns all words
+    /// 
+    /// Unlike the IntoIterator trait, does not consume the Wordlist
+    fn iter(&'a self) -> Self::IterType;
+
+    /// Given a word, returns true if the word is contained within this `Wordlist`
+    fn includes_word(&self, word: &str) -> bool;
 }
 
-impl<'a> FromIterator<&'a str> for Wordlist<'a> {
-    
-    /// Constructs a `Wordlist` from an iterator over `&str`s
-    /// 
-    /// Each value returned by the iterator should be one and only one word.
-    /// If a value contains two words, they will be interpreted as one word with a
-    /// space character as one of the letters
-    /// 
-    /// This is the underlying method that all other `Wordlist` constructors use
-    fn from_iter<T: IntoIterator<Item = &'a str >>(iter: T) -> Self {
-        Self { word_set: WordlistStorage::Borrowed(iter.into_iter().collect())}
+/// A [Wordlist] implementor that borrows its words
+/// 
+/// Useful for creating a `Wordlist` from data that already exists
+/// (such as a `&'static str` or pre-existing `String`)
+pub struct BorrowedWordList<'a> {
+    word_vec: Vec<&'a str>
+}
+
+impl<'a> BorrowedWordList<'a> {
+    /// Construct a new StrWordList from an iterator of `&str`
+    pub fn new(word_iter: impl IntoIterator<Item = &'a str>) -> Self
+    {
+        Self { word_vec: word_iter.into_iter().collect() }
     }
 }
 
-impl<'a> IntoIterator for &'a Wordlist<'a>{
-    type Item = &'a str;
-    type IntoIter = Box<dyn Iterator<Item = Self::Item> + 'a>;
-    
-    /// Returns an iterator over 
-    fn into_iter(self) -> Self::IntoIter {
-        match &self.word_set {
-            WordlistStorage::Borrowed(vec) => {
-                Box::new(vec.iter().map(|p|{*p}))
-            },
-            WordlistStorage::Owned(vec) => {
-                Box::new(vec.iter().map(|p|{p.as_str()}))
-            }
-        }
+impl<'a> FromIterator<&'a str> for BorrowedWordList<'a>{
+    fn from_iter<T: IntoIterator<Item = &'a str>>(iter: T) -> Self {
+        Self::new(iter)
     }
 }
 
-impl<'a> Wordlist<'a> {
+impl<'a> Wordlist<'a> for BorrowedWordList<'a>{
+    type IterType = std::iter::Map<std::slice::Iter<'a, &'a str>, fn(&&'a str) -> &'a str>;
 
-    /// Similar to [Vec]'s `iter` function
-    /// 
-    /// Returns the result of `IntoIterator::into_iter(&WordList)`
-    pub fn iter(&'a self) -> Box<dyn Iterator<Item = &'a str> + 'a>
-    {
-        self.into_iter()
+    fn includes_word(&self, word: &str) -> bool {
+        self.word_vec.contains(&word)
     }
 
-    /// Constructs a `Wordlist` from a `&str`
-    ///
-    /// `content` should be a `&str` containing a newline-delimited list of words.
-    /// Each line should contain one word. If a line contains two words, they will be 
-    /// treated as a single word with the space character interpreted as a letter.
-    /// 
-    /// If `content` doesn't contain any newlines, it is assumed to be a single word.
-    pub fn from_str(content: &'a str) -> Self
-    {
-        // because Wordlist implements the FromIterator trait,
-        // we can just call collect on the lines iterator        
-        content.lines().collect()
-    }
-
-    /// Constructs a `Wordlist` from an open text file
-    /// 
-    /// `wordlist_file` should be a [Path](std::path::Path) to an existing text (UTF-8) file.
-    /// Each line of the file should contain one word. 
-    /// If a line contains two words, they will be treated as a
-    /// single word with the space character interpreted as a letter.
-    /// 
-    /// If the content of `wordlist_file` doesn't contain any newlines,
-    /// it is assumed to be a single word.
-    pub fn from_file(wordlist_file_path: &path::Path) -> io::Result<Self>
-    {
-        let lines_iter = io::BufReader::new(fs::File::open(wordlist_file_path)?).lines();
-        let mut word_vec = Vec::new();
-        for line in lines_iter{
-            word_vec.push(line?);
-        }
-
-        Ok(Self{word_set: WordlistStorage::Owned(word_vec)})
+    fn iter(&'a self) -> Self::IterType {
+        self.word_vec.iter().map(|d: &&'a str|->&'a str{*d})
     }
 }
 
 #[cfg(test)]
-mod tests {
-    use super::Wordlist;
+mod borrowedwordlist_tests{
+    use super::{BorrowedWordList, Wordlist};
 
     #[test]
-    fn test_iteration()
+    fn test_includes_word()
     {
-        fn assertion<'a>(mut word_iterator: Box<dyn Iterator<Item = &'a str> + 'a>)
-        {
-            assert_eq!(word_iterator.next(), Some("a"));
-            assert_eq!(word_iterator.next(), Some("aa"));
-            assert_eq!(word_iterator.next(), Some("aaa"));
-            assert_eq!(word_iterator.next(), Some("aaaa"));
+        let list = BorrowedWordList::new(["a","b","c"]);
+        assert!(list.includes_word("a"));
+        assert!(!list.includes_word("not in list"))
+    }
+
+    #[test]
+    fn test_to_from_iter()
+    {
+        const WORD_ARRAY: [&str; 3] = ["a", "b", "c"];
+        use core::iter;
+        let list_from_iter: BorrowedWordList = WORD_ARRAY.into_iter().collect();
+        let list_from_new = BorrowedWordList::new(WORD_ARRAY);
+    
+        let first_iter = list_from_iter.iter();
+        let second_iter = list_from_new.iter();
+
+        for (first, second) in iter::zip(first_iter, second_iter){
+            assert_eq!(first, second);
         }
-
-        let wordstring = "a\naa\naaa\naaaa";
-        let wordlist = Wordlist::from_str(wordstring);
-        let iter_one = wordlist.iter();
-        
-        assertion(iter_one);
-
-        let iter_two = wordlist.iter();
-        assertion(iter_two);
-
-
     }
 }
